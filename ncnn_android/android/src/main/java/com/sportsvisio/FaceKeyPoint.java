afd16a7cf109c1aa133d368825bc8c771e36914d
Tomás Sasovsky <tomas@sportsvisio.com>
2022-12-28 23:49:44 -0300

    chore: add cpp folder and setup project

---
+++ ncnn_android/android/src/main/java/com/sportsvisio/FaceKeyPoint.java
@@ -0,0 +1,27 @@
+package com.sportsvisio;
+
+class FaceKeyPoint {
+    public float x, y;
+
+    public FaceKeyPoint(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+}
