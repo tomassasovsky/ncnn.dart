915d4a2fad7483a663c2f0ac5c7fcb627d75d6d7
Tomás Sasovsky <tomas@sportsvisio.com>
2022-12-30 17:37:45 -0300

    chore: copyright marker on all our files

--- ncnn_android/android/src/main/java/com/sportsvisio/FaceKeyPoint.java
+++ ncnn_android/android/src/main/java/com/sportsvisio/FaceKeyPoint.java
@@ -1,3 +1,10 @@
+// Copyright (c) 2022, SportsVisio, Inc.
+// https://sportsvisio.com/
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 package com.sportsvisio;
 
 class FaceKeyPoint {
--- ncnn_android/android/src/main/java/com/sportsvisio/OpenPoseKeyPoint.java
+++ ncnn_android/android/src/main/java/com/sportsvisio/OpenPoseKeyPoint.java
@@ -1,5 +1,11 @@
-package com.sportsvisio;
+// Copyright (c) 2022, SportsVisio, Inc.
+// https://sportsvisio.com/
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
+package com.sportsvisio;
 
 public class OpenPoseKeyPoint {
     public float[] x;
